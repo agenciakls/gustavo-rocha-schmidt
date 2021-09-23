aead772915366cfbb4761a6fca18921786c9854c
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2021-09-22 21:46:55 -0300

    Primeiro Commit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php 
+get_header(); 
+
+get_template_part('templates/home', 'signature');
+get_template_part('templates/home', 'offers');
+get_template_part('templates/ads', 'home-1');
+get_template_part('templates/home', 'tips');
+get_template_part('templates/ads', 'home-2');
+get_template_part('templates/home', 'services');
+get_template_part('templates/home', 'roleta');
+
+get_footer(); 
+?>
