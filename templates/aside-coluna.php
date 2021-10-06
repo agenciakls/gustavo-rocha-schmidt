306f45eaf2e64de5a81d5c038564d50e9bb9ff56
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2021-10-06 00:32:12 -0300

    Coluna - Aside

--- templates/aside-coluna.php
+++ templates/aside-coluna.php
@@ -10,38 +10,45 @@
             </div>
         </div>
     </div>
-    <div class="part-categorias">
-        <h3>Filtrar por categorias</h3>
-        <div class="content-categorias">
-            <?php
-            $postType = get_post_type();
-            $terms = get_terms( array(
-                'taxonomy' => $postType,
-                'hide_empty' => false,
-            ) );
-            foreach ($terms as $term) {
-                $singleTerm = get_term_link($term->term_id, $postType);
-                ?><a href="<?php echo $singleTerm; ?>"><div class="single-categorias"><i class="fas fa-chevron-right"></i> <?php echo $term->name; ?></div></a><?php
-            }   
-            ?>
+    <?php 
+    if (get_post_type() && !is_search()) {
+        ?>
+        
+        <div class="part-categorias">
+            <h3>Filtrar por categorias</h3>
+            <div class="content-categorias">
+                <?php
+                $postType = get_post_type();
+                $terms = get_terms( array(
+                    'taxonomy' => $postType,
+                    'hide_empty' => false,
+                ) );
+                foreach ($terms as $term) {
+                    $singleTerm = get_term_link($term->term_id, $postType);
+                    ?><a href="<?php echo $singleTerm; ?>"><div class="single-categorias"><i class="fas fa-chevron-right"></i> <?php echo $term->name; ?></div></a><?php
+                }
+                ?>
+            </div>
         </div>
-    </div>
-    <div class="part-tags">
-        <h3>Tags</h3>
-        <div class="content-tags">
-            <?php
-            $postType = get_post_type();
-            $terms = get_terms( array(
-                'taxonomy' => $postType,
-                'hide_empty' => false,
-            ) );
-            foreach ($terms as $term) {
-                $singleTerm = get_term_link($term->term_id, $postType);
-                ?><a href="<?php echo $singleTerm; ?>"><div class="single-tags"><?php echo $term->name; ?></div></a><?php
-            }
-            ?>
+        <div class="part-tags">
+            <h3>Tags</h3>
+            <div class="content-tags">
+                <?php
+                $postType = get_post_type();
+                $terms = get_terms( array(
+                    'taxonomy' => $postType,
+                    'hide_empty' => false,
+                ) );
+                foreach ($terms as $term) {
+                    $singleTerm = get_term_link($term->term_id, $postType);
+                    ?><a href="<?php echo $singleTerm; ?>"><div class="single-tags"><?php echo $term->name; ?></div></a><?php
+                }
+                ?>
+            </div>
         </div>
-    </div>
+        <?php
+    }
+    ?>
     <div class="part-tags">
         
     </div>
