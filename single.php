a4a3c45787ec7d5cb58dca366c42425804cae5c2
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2021-10-08 05:10:12 -0300

    Box de eventos

--- single.php
+++ single.php
@@ -7,7 +7,7 @@ get_header();
 			<div class="col-md-7">
 				<div class="content-single">
 					<h1><?php echo get_the_title(); ?></h1>
-					<p class="text-muted"><small><i class="far fa-clock"></i> <?php echo get_the_date(' d/m/Y H:i '); ?></small> <?php 
+					<p class="text-muted py-2"><small><i class="far fa-clock"></i> <?php echo get_the_date(' d/m/Y H:i '); ?></small> <?php 
 					$postType = get_post_type();
 					$terms = get_the_terms( $post, $postType . 'cat' );
 					$count = (is_array($terms)) ? count($terms) : 0; 
@@ -21,6 +21,7 @@ get_header();
 					}
 					if (get_field('link') && $postType == 'livros') { ?><a href="<?php echo get_field('link'); ?>" target="_blank"><button class="button secundary">Comprar</button></a><?php } 
 					?>
+					</p>
 					<div class="content-text">
 						<?php 
 						if (have_posts()) {
@@ -28,6 +29,42 @@ get_header();
 								the_post();
 								if (has_post_thumbnail()) {
 									?><img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" class="rounded float-md-right w-100 img-fluid my-2"><?php
+								}
+								if ($postType == 'eventos') {
+									$dataHorario = get_field('data_horario');
+									$dia = strftime("%d", strtotime($dataHorario));
+									$mes = strftime("%b", strtotime($dataHorario));
+									$ano = strftime("%Y", strtotime($dataHorario));
+									$hora = strftime("%k", strtotime($dataHorario));
+									?>
+										<div class="detail-eventos my-3 d-flex row align-items-stretch">
+											<div class="col-md-2 col-date">
+												<div class="box-event">
+													<div class="date-day"><?php echo $dia; ?></div>
+													<div class="date-month"><?php echo $mes; ?></div>
+													<div class="date-year"><?php echo $ano; ?></div>
+												</div>
+											</div>
+											<div class="col-md-2">
+												<div class="box-event">
+													<div class="date-hour">
+														<i class="far fa-clock"></i> <?php echo $hora; ?>h
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="box-event">
+													<div class="content-address">
+														<?php echo get_field('endereco'); ?>
+													</div>
+												</div>
+												
+											</div>
+											<div class="col-md-4">
+												<?php if (get_field('link')) { ?><a href="<?php echo get_field('link'); ?>" target="_blank"><div class="box-event inverted spacials">Faça sua inscrição</div></a><?php } ?>
+											</div>
+										</div>
+											<?php
 								}
 								the_content();
 							}
